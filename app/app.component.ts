4616b36e7d950a593aec35292d0e0f75a734c1b6
Rachelle Gray <rgray@bltomato.com>
2016-06-28 12:03:07 -0700

    Angular 2 - TOH - master/details:  - create array of heroes, of type Hero  - define hero array property  - create public property in AppComponent that exposes heroes for binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -11,8 +11,13 @@ export class Hero {
     <h1>{{title}}</h1>
     <article>
         <h2>{{hero.name}} details!</h2>
-        <label>id: <input readonly="true" value="{{hero.id}}"></label><br>
-        <label>name: <input type="text" [(ngModel)]="hero.name" placeholder="Add name here"></label>
+        <label>id: 
+            <input readonly="true" value="{{hero.id}}">
+        </label>
+        <br>
+        <label>name: 
+            <input type="text" [(ngModel)]="hero.name" placeholder="Add name here">
+        </label>
     </article>
     `
 })
@@ -23,4 +28,18 @@ export class AppComponent {
         id: 1,
         name: 'Windstorm'
     };
+    public heroes = HEROES;
 }
+
+const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
