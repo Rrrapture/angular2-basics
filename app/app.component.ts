8d7a26bc117989bc7fee8cda38c0fb5ef3441b8f
Rachelle Gray <rgray@bltomato.com>
2016-06-27 17:59:02 -0700

    Angular 2 - Tour of Heroes tutorial - Hero object:  - create Hero class with id and name properties  - refactor component's hero property to be of type Hero, initalized with id and name  - update binding in template to get hero's name property

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+	id: number;
+	name: string;
+}
+
 @Component({
 	selector: 'my-app',
-	template: '<h1>{{title}}</h1><article>{{hero}} details!</article>'
+	template: '<h1>{{title}}</h1><article>{{hero.name}} details!</article>'
 })
 
 export class AppComponent {
 	title = 'Tour of Heroes';
-	hero = 'Windstorm';
+	hero: Hero = {
+		id: 1,
+		name: 'Windstorm'
+	};
 }
