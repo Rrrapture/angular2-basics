e207a2859ad19de5d24a25a58a9208381d558a5e
Rachelle Gray <rgray@bltomato.com>
2016-06-28 11:18:39 -0700

    Angular 2 - Tour of Heroes tutorial - convert indentation to spaces to remain consistent with all project files

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,26 +1,26 @@
 import { Component } from '@angular/core';
 
 export class Hero {
-	id: number;
-	name: string;
+    id: number;
+    name: string;
 }
 
 @Component({
-	selector: 'my-app',
-	template: `
-	<h1>{{title}}</h1>
-	<article>
-		<h2>{{hero.name}} details!</h2>
-		<label>id: <input readonly="true" value="{{hero.id}}"></label><br>
-		<label>name: <input type="text" value="{{hero.name}}"></label>
-	</article>
-	`
+    selector: 'my-app',
+    template: `
+    <h1>{{title}}</h1>
+    <article>
+        <h2>{{hero.name}} details!</h2>
+        <label>id: <input readonly="true" value="{{hero.id}}"></label><br>
+        <label>name: <input type="text" value="{{hero.name}}"></label>
+    </article>
+    `
 })
 
 export class AppComponent {
-	title = 'Tour of Heroes';
-	hero: Hero = {
-		id: 1,
-		name: 'Windstorm'
-	};
+    title = 'Tour of Heroes';
+    hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
 }
