d8375eacfcd4efe7a623acb840c85303c6511df3
Rachelle Gray <rgray@bltomato.com>
2016-06-28 18:38:13 -0700

    Angular 2 - TOH tutorial - services (wip):  - move HEROES array into mock-heroes file  - change heroes propert in AppComponent to the Hero array  - import hero service and mock-heroes  - ngOnInit lifecycle hook  - import and implement ngOnInit lifecycle hook  - write ngOnInit method and call this.getHeroes function

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
 
 @Component({
     selector: 'my-app',
@@ -65,27 +67,26 @@ import { HeroDetailComponent } from './hero-detail.component';
             border-radius: 4px 0 0 4px;
         }
     `],
-    directives: [HeroDetailComponent]
+    directives: [HeroDetailComponent],
+    providers: [HeroService]
 
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'Tour of Heroes';
-    heroes = HEROES;//public disappears for some reason
+    heroes : Hero[];
     selectedHero: Hero;
+
+    ngOnInit() {
+        this.getHeroes();
+    }
+    constructor(private heroService: HeroService) {}
+
+    getHeroes() {
+        this.heroes = this.heroService.getHeroes();
+    }
+
 
     onSelect(hero: Hero) { this.selectedHero = hero; }
 }
 
-const HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
---
+++ app/hero.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { HEROES } from './mock-heroes';
+
+@Injectable()
+
+export class HeroService {
+    getHeroes() {
+        return HEROES;
+    }
+}
