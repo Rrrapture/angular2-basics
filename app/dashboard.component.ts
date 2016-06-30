86a7edcfd067888ef52e8aea235b6bbdc29f02a0
Rachelle Gray <rgray@bltomato.com>
2016-06-30 16:38:59 -0700

    Angular 2 - TOH tutorial - routing:  - migrate template html string in DashboardComponent to new template html file  - replace template metadata in AppComponent with templateUrl property  - in template, use ngFor repeater directive to display each item in the list  - write gotoDetail method stub

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
-	selector: 'my-dashboard',
-	template '<h3>My Dashboard</h3>'
+    selector: 'my-dashboard',
+    templateUrl: 'app/dashboard.component.html'
 })
 
-export class DashboardComponent { }
+export class DashboardComponent implements OnInit {
+    heroes: Hero[] = [];
+
+    constructor(private heroService: HeroService) { }
+
+    ngOnInit () {
+        this.heroService.getHeroes()
+            .then(heroes => this.heroes = heroes.slice(1, 5));
+    }
+
+    gotoDetail() {
+        //not implemented yet
+    }
+}
