c00290f68f2c00fccf622dcab14d36c7e83d1540
Rachelle Gray <rgray@bltomato.com>
2016-07-01 14:37:47 -0700

    Angular 2 - TOH tutorial - routing  - import router and inject it into the constructor

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
@@ -11,14 +12,18 @@ import { HeroService } from './hero.service';
 export class DashboardComponent implements OnInit {
     heroes: Hero[] = [];
 
-    constructor(private heroService: HeroService) { }
+    constructor(
+        private router: Router,
+        private heroService: HeroService
+    ) { }
 
     ngOnInit () {
         this.heroService.getHeroes()
             .then(heroes => this.heroes = heroes.slice(1, 5));
     }
 
-    gotoDetail() {
-        //not implemented yet
+    gotoDetail(hero: Hero) {
+        let link = ['/detail', hero.id];
+        this.router.navigate(link);
     }
 }
