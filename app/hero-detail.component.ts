b86e4e8deea675982ba39ddccb19f3e556366f49
Rachelle Gray <rgray@bltomato.com>
2016-07-05 17:20:46 -0700

    Angular 2 - TOH - routing:  - add styles provided from angular tutorial to hero detail component,  - with go-back link styles

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -6,7 +6,8 @@ import { HeroService } from './hero.service';
 
 @Component({
     selector: 'my-hero-detail',
-    templateUrl: 'app/hero-detail.component.html'
+    templateUrl: 'app/hero-detail.component.html',
+    styleUrls: ['app/hero-detail.component.css']
 })
 
 export class HeroDetailComponent implements OnInit, OnDestroy {
