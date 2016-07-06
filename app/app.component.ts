4c8fda74c3888763221d2c91bf441b9dc58f1196
Rachelle Gray <rgray@bltomato.com>
2016-07-05 18:09:35 -0700

    Angular 2 - TOH - routing:  - move app component styles to their own file, comment out seed styles in styles.css  - add routerLinkActive directive to the heroes nav link

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,10 +9,11 @@ import { HeroService } from './hero.service';
         <h1>{{title}}</h1>
         <nav>
             <a [routerLink]="['/dashboard']" routerLinkActive="active">Dashboard</a>
-            <a [routerLink]="['/heroes']">Heroes</a>
+            <a [routerLink]="['/heroes']" routerLinkActive="active">Heroes</a>
         </nav>
         <router-outlet></router-outlet>
     `,
+    styleUrls: ['app/app.component.css'],
     directives: [ROUTER_DIRECTIVES],
     providers: [HeroService]
 })
