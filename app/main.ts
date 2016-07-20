01dafaca4e7aff25392ea26d7a715f00e7755bf5
Rachelle Gray <rgray@bltomato.com>
2016-07-19 18:08:56 -0700

    Angular 2 - TOH - http:  - add in-memory web API  - HTTP client fetches and saves data from a mock service

--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,10 @@
+// imports for loading & configuring in-memory web api
+import { XHRBackend} from '@angular/http';
+
+import { InMemoryBackendService, SEED_DATA } from 'angular2-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
+// usual bootstrapping imports
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { HTTP_PROVIDERS } from '@angular/http';
 
@@ -6,5 +13,7 @@ import { APP_ROUTER_PROVIDERS } from './app.routes';
 
 bootstrap(AppComponent, [
 	APP_ROUTER_PROVIDERS,
-	HTTP_PROVIDERS
+	HTTP_PROVIDERS,
+	{ provide: XHRBackend, useClass: InMemoryBackendService }, //in-mem server
+	{ provide: SEED_DATA, useClass: InMemoryDataService } //in-mem server data
 ]);
